955d05093bf8c0aa6a5f94c1c4f6a46b2c7c4b15
bradoooya <yuukikdy@gmail.com>
2024-07-16 01:35:42 +0100

    refresh test.py path

--- test.py
+++ test.py
@@ -1,10 +1,28 @@
 from pathlib import Path
 from ultralytics import YOLO
+import os
+from datetime import datetime
 
+#モデルのロード
 model = YOLO('./model/best.pt')
 
+#画像ディレクトリのパス
+image_dir = Path("./data/images/for_pred")
 
-jpg_files = Path(".").glob("*.jpg")
+#予想結果の保存先のパス
+timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+save_dir = f'./results/run_pred_test/{timestamp}'
+os.makedirs(save_dir, exist_ok=True)
+
+#画像ディレクトリからjpgファイルを取得
+jpg_files = image_dir.glob("*.jpg")
 
 for file in jpg_files:
-    model.predict(f'./{file}', save=True)
+    #推論の実行と結果の保存
+    results = model.predict(str(file))
+
+    # 各結果を保存
+    for i, result in enumerate(results):
+        save_path = os.path.join(save_dir, f'{file.stem}_result_{i}_.jpg')
+        result.save(save_path)
+
