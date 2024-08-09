208e2e6771fe735b9a19ff1690b03cdece67a73e
bradoooya <yuukikdy@gmail.com>
2024-08-09 03:02:13 +0100

    display count fin

--- src/pred/yolov8_webcam_countonly.py
+++ src/pred/yolov8_webcam_countonly.py
@@ -1,4 +1,9 @@
 from ultralytics import YOLO
+import sys
+sys.path.append("/home/houei/Desktop/development/yorov8/project_ML/src")
+
+from utils.utils import LCD, LCDMessage
+
 # モデルを選択
 model = YOLO('./model/best.pt')
 
@@ -9,12 +14,18 @@ model.to("cpu")
 confidence_threshold = 0.7
 results = model(source=0 ,conf=confidence_threshold ,show=False, stream=True)
 
-"""
-for i in enumerate(results):
-    print(i)
-"""
-
-# 検出結果の個数をプリント
-for result in results:
-    num_detections = len(result.boxes)  # 検出されたバウンディングボックスの数を取得
-    print(f"Number of detections: {num_detections}")
+try:
+    lcd =  LCD()
+    lcd_pred = LCDMessage(lcd)
+    for result in results:
+        num_detections = len(result.boxes)  # 検出されたバウンディングボックスの数を取得
+        text = f"Number of detections: {num_detections}"
+        print(text)
+        lcd_pred.display_message(text, display_duration=10)
+except Exception as e:
+    print(e)
+except ValueError as e:
+    print(e)
+finally:
+    lcd.clear()
+    
